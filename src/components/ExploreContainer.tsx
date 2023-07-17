9105e4548ca85b3a3ba0002ff6d32f0f174e9d48
Yaroslav Shulika <legendar@gmail.com>
2023-07-17 17:20:16 +0300

    example

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,25 @@
+import { IonInput, IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get('username'),
+      password: data.get('password'),
+    });
+  };
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <form onSubmit={handleSubmit}>
+        <IonInput label="Username" name="username" labelPlacement="stacked" />
+        <IonInput label="Password" name="password" type="password" labelPlacement="stacked" />
+        <IonButton type="submit">Submit</IonButton>
+      </form>
     </div>
   );
 };
